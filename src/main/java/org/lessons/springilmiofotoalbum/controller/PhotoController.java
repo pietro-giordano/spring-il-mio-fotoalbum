36b5815504cf3f22bb95a1c8a8c142ab6684ab1e
Pietro Giordano <giopi87@gmail.com>
2023-07-06 13:02:15 +0200

    avanzamento PhotoController, PhotoService e index photos

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,6 +1,9 @@
 package org.lessons.springilmiofotoalbum.controller;
 
+import org.lessons.springilmiofotoalbum.service.PhotoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/photos")
 public class PhotoController {
 
+      @Autowired
+      private PhotoService photoService;
+
       @GetMapping
-      public String index() {
+      public String index(Model model) {
+            model.addAttribute("photos", photoService.getPhotos());
             return "/photos/index";
       }
 }
--- src/main/java/org/lessons/springilmiofotoalbum/service/PhotoService.java
+++ src/main/java/org/lessons/springilmiofotoalbum/service/PhotoService.java
@@ -1,7 +1,19 @@
 package org.lessons.springilmiofotoalbum.service;
 
+import org.lessons.springilmiofotoalbum.model.Photo;
+import org.lessons.springilmiofotoalbum.repository.PhotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PhotoService {
+
+      @Autowired
+      private PhotoRepository photoRepository;
+
+      public List<Photo> getPhotos() {
+            return photoRepository.findAll();
+      }
 }
