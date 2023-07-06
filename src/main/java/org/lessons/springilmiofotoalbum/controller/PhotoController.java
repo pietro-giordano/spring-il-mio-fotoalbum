fb958e3f19ff2566db0109cde94fad175aa5a520
Pietro Giordano <giopi87@gmail.com>
2023-07-06 11:44:31 +0200

    stesura bozze template

--- src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/lessons/springilmiofotoalbum/controller/PhotoController.java
@@ -1,9 +1,15 @@
 package org.lessons.springilmiofotoalbum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/photos")
 public class PhotoController {
+
+      @GetMapping
+      public String index() {
+            return "/photos/index";
+      }
 }
