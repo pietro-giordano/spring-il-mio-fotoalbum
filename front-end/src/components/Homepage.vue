416a5bbc26b5a89b94102a0d47a508ded8f911a4
Pietro Giordano <giopi87@gmail.com>
2023-07-08 10:36:52 +0200

    form/offcanvas messaggio in frontend

--- front-end/src/components/Homepage.vue
+++ front-end/src/components/Homepage.vue
@@ -61,6 +61,30 @@ export default {
                         </div>
                   </div>
             </div>
+
+            <!-- Offcanvas con form messaggi -->
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                  <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasRightLabel">Inviami un messaggio</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                  </div>
+                  <div class="offcanvas-body">
+
+                        <form action="POST">
+                              <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Indirizzo email</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                          placeholder="name@example.com">
+                              </div>
+                              <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Testo messaggio</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                              </div>
+                              <button type="submit" class="btn btn-primary">Invia</button>
+                        </form>
+
+                  </div>
+            </div>
       </div>
 </template>
 
--- front-end/src/components/Navbar.vue
+++ front-end/src/components/Navbar.vue
@@ -8,18 +8,19 @@ export default {
 <template>
       <nav class="navbar navbar-expand-lg container">
             <div class="container-fluid">
-                  <a class="navbar-brand" href="#">Album fotografico</a>
+                  <a class="navbar-brand" href="/">Album fotografico</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                   </button>
-                  <div class="collapse navbar-collapse" id="navbarNav">
+                  <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                         <ul class="navbar-nav">
                               <li class="nav-item">
-                                    <a class="nav-link active" href="#">Foto</a>
+                                    <a class="nav-link" href="http://localhost:8080">Area riservata</a>
                               </li>
-                              <li class="nav-item">
-                                    <a class="nav-link" href="#">Contatta</a>
+                              <li class="nav-item nav-link" role='button' data-bs-toggle="offcanvas"
+                                    data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                                    Contattami
                               </li>
                         </ul>
                   </div>
